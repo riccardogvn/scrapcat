bfebc9218621681bc0ac5c6154fd0b98080724c0
riccardogvn <57218092+riccardogvn@users.noreply.github.com>
2024-02-09 15:27:11 +0100

    First upload

---
+++ setup/config.py
@@ -0,0 +1,9 @@
+###last_update 2024_02_05
+LAST_EXPERT_ID = 1407
+LIST_OF_EXPERTS = 'datasets/catawiki_experts.json'
+CATEGORIES_OK = 'datasets/titlesokcata.json'
+CATEGORIES_NOT_OK = 'datasets/titlesnot.json'
+LIST_OF_OBJECTS = 'datasets/cataobjects.json'
+DICT_OF_IDS = 'datasets/cataobj.json'
+CATA_LOTS = 'datasets/catalots.json'
+
